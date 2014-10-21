8397213d2ccff8921393946813578e3c28693d09
Grant Heffernan <heffergm@gmail.com>
2014-10-21 15:02:45 -0400

    attributes readability

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@
 
 default[:tilequeue][:cfg_path]                                  = '/etc/tilequeue'
 default[:tilequeue][:cfg_file]                                  = 'config.yaml'
+default[:tilequeue][:pip_requirements]                          = "#{Chef::Config[:file_cache_path]}/pip-requirements.txt"
 
 # common configuration with tilestache
 default[:tilequeue][:tilestache][:user]                         = node[:tilestache][:user]      || 'tilestache'
@@ -18,11 +19,13 @@ default[:tilequeue][:aws][:s3][:bucket]                         = ''
 default[:tilequeue][:aws][:s3][:path]                           = ''
 default[:tilequeue][:aws][:credentials][:aws_access_key_id]     = ''
 default[:tilequeue][:aws][:credentials][:aws_secret_access_key] = ''
+
 default[:tilequeue][:tiles][:metro_extract][:zoom_filter]       = 10
 default[:tilequeue][:tiles][:zoom_start]                        = 0
 default[:tilequeue][:tiles][:zoom_until]                        = 15
 default[:tilequeue][:tiles][:expired]                           = ''
+
 default[:tilequeue][:tilestache][:config]                       = "#{default[:tilequeue][:tilestache][:cfg_path]}/#{default[:tilequeue][:tilestache][:cfg_file]}"
 default[:tilequeue][:tilestache][:formats]                      = %w(json vtm)
+
 default[:tilequeue][:logging][:config]                          = "#{default[:tilequeue][:cfg_path]}/logging.conf"
-default[:tilequeue][:pip_requirements]                          = "#{Chef::Config[:file_cache_path]}/pip-requirements.txt"
