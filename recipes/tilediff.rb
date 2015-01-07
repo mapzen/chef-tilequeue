73b4570d16a8bc03ec9791ebb9976dfabdc16aa9
Robert Marianski <rob@marianski.com>
2015-01-07 15:06:48 -0500

    Place expired tile files into separate location

--- recipes/tilediff.rb
+++ recipes/tilediff.rb
@@ -7,7 +7,7 @@ package 'redis-tools'
 
 include_recipe 'tilequeue'
 
-template node[:tilequeue][:tilediff][:intersect][:script][:path] do
+template node[:tilequeue][:tilediff][:script][:path] do
   source 'tilequeue-tilediff.sh.erb'
   mode 0755
 end
@@ -17,6 +17,6 @@ cron 'tilequeue diff' do
   hour    node[:tilequeue][:tilediff][:cron][:hour]
   day     node[:tilequeue][:tilediff][:cron][:day]
   user    node[:tilequeue][:tilediff][:cron][:user]
-  command "#{node[:tilequeue][:tilediff][:intersect][:script][:path]} >>#{node[:tilequeue][:tilediff][:intersect][:script][:output]} 2>&1"
+  command "#{node[:tilequeue][:tilediff][:script][:path]} >>#{node[:tilequeue][:tilediff][:script][:output]} 2>&1"
   only_if { node[:tilequeue][:tilediff][:cron][:enabled] == true }
 end
