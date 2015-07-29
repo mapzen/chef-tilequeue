e69ff7c79d6978100775a36270dc5e93d115350b
Robert Marianski <rob@marianski.com>
2015-07-29 12:01:30 -0400

    Bump mapbox-vector-tile version

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,7 +14,7 @@ default[:tilequeue][:pip_requirements] = %w(
   argparse==1.2.1
   boto==2.33.0
   hiredis==0.1.5
-  mapbox-vector-tile==0.0.10
+  mapbox-vector-tile==0.0.11
   ModestMaps==1.4.6
   Pillow==2.6.1
   protobuf==2.6.0
