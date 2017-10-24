aee71e96d572035962eaaebaed6e06602292115b
Robert Marianski <rob@marianski.com>
2017-10-24 15:39:57 -0400

    Update runit logging

--- recipes/rawr_processor.rb
+++ recipes/rawr_processor.rb
@@ -5,6 +5,6 @@ include_recipe 'runit::default'
 runit_service 'rawr' do
   action          [:enable]
   log             true
-  default_logger  true
+  default_logger  false
   sv_timeout      node[:tilequeue][:runit][:timeout]
 end
